fe3ee0d4cac8557908efdf92098df44f113b37e5
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-21 10:08:09 +0100

    Added location checker and map function to restaurant data test

--- src/location_test.js
+++ src/location_test.js
@@ -1,14 +1,12 @@
 const axios = require('axios');
 
 const encodedParams = new URLSearchParams();
+encodedParams.set('q', 'manchester');
 encodedParams.set('language', 'en_US');
-encodedParams.set('limit', '500');
-encodedParams.set('location_id', '186338');
-encodedParams.set('currency', 'GBP');
 
 const options = {
   method: 'POST',
-  url: 'https://worldwide-restaurants.p.rapidapi.com/search',
+  url: 'https://worldwide-restaurants.p.rapidapi.com/typeahead',
   headers: {
     'content-type': 'application/x-www-form-urlencoded',
     'X-RapidAPI-Key': '2a7af80d8bmsh9bd131e2560e931p17d3fejsn868a77223011',
@@ -17,25 +15,14 @@ const options = {
   data: encodedParams,
 };
 
-async function getRestaurants() {
+async function getLocation() {
 
 try {
 	const response = await axios.request(options);
-  const restaurants = response.data.results.data;
-
-
-  for (let i = 0; i < restaurants.length; i++) {
-    console.log(restaurants[i].name);
-    console.log(i);
-  }
-  
-
-	// console.log(restaurants[0]);
+	console.log(response.data.results.data);
 } catch (error) {
 	console.error(error);
 }
 }
 
-
-
-getRestaurants();
+getLocation();
